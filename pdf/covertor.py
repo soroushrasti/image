31bfd554118278be8ec1b3be9cb7af36f33dd60f
Soroush Rasti <rasti.soroush@gmail.com>
2020-08-27 18:07:47 +0200

    added pdf.py-1

--- pdf/covertor.py
+++ pdf/covertor.py
@@ -3,3 +3,12 @@ import PyPDF2
 temp=PyPDF2.PdfFileReader(open('./files/all.pdf'),'rb')
 watermark=PyPDF2.PdfFileReader(open('./files/wtr.pdf'),'rb')
 output=PyPDF2.PdfFileWriter()
+
+for i in range(temp.getNumPages()):
+    page=temp.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.add(page)
+
+ with open('water-all.pdf','wb') as file:
+     output.write(file)   
+
